ae4309a9990659eb6e72f08d7aa95a13790667ae
Daler Davronov <d.davronov@ihsph.org>
2023-11-29 20:52:42 -0500

    v2

--- main.tf
+++ main.tf
@@ -1,40 +1,9 @@
-terraform {
-  cloud {
-    organization = "Ziyotek_Terraform_Class_Summer_Cloud"
-
-    workspaces {
-      name = "Nov_17_Handson_Lab"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
     Name = "main-vpc"
-  }
-}
-
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block             = "10.0.1.0/24"
-  availability_zone       = "us-east-1a" 
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet"
   }
 }
 
@@ -45,10 +14,58 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_vpc_attachment" "main" {
-  vpc_id          = aws_vpc.main.id
-  internet_gateway_id = aws_internet_gateway.main.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
 }
+
+resource "aws_route_table_association" "public" {
+  for_each = var.subnets
+  subnet_id      = aws_subnet.subnets[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "subnets" {
+  for_each = var.subnets
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+
+  tags = {
+    Name = "subnet-${each.key}"
+  }
+}
+
+
+# resource "aws_network_interface" "multi-ip" {
+#   subnet_id   = aws_subnet.subnets.id
+#   private_ips = ["10.0.0.10", "10.0.0.11", "10.0.0.12"]
+# }
+
+# resource "aws_eip" "one" {
+#   domain                    = "vpc"
+#   network_interface         = aws_network_interface.multi-ip.id
+#   associate_with_private_ip = "10.0.0.10"
+# }
+
+# resource "aws_eip" "two" {
+#   domain                    = "vpc"
+#   network_interface         = aws_network_interface.multi-ip.id
+#   associate_with_private_ip = "10.0.0.11"
+# }
+# resource "aws_eip" "three" {
+#   domain                    = "vpc"
+#   network_interface         = aws_network_interface.multi-ip.id
+#   associate_with_private_ip = "10.0.0.12"
+# }
 
 resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
@@ -80,11 +97,41 @@ resource "aws_security_group" "allow_http" {
 }
 
 
+# resource "aws_instances" "my-instances" {
+#   count         = var.instance_count
+#   ami           = lookup(var.ami,var.aws_region)
+#   instance_type = var.instance_type
+#   key_name      = aws_key_pair.terraform-demo.key_name
+#   user_data     = file("install_apache.sh")
+
+#   tags = {
+#     Name  = "Terraform-${count.index + 1}"
+#   }
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 resource "aws_instance" "web" {
   ami             = "ami-0230bd60aa48260c6"
   instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public.id
-  security_group  = [aws_security_group.allow_http.id]
+  # subnet_id       = aws_subnet.public.id
+  security_groups = [aws_security_group.allow_http.id]
 
   user_data = <<-EOF
               #!/bin/bash
